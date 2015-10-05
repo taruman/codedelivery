9694f44e63438730e6f0cbc91744fd74d86490fa
Taruman <taru931@gmail.com>
2015-10-05 02:27:22 -0300

    criando middleware para controlar permissões das rotas

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace CodeDelivery\Http\Middleware;
+
+use Closure;
+use Auth;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        if(!Auth::check())
+        {
+            return redirect('/auth/login');
+        }
+
+        if(Auth::user()->role != "admin")
+        {
+            return redirect('/auth/login');
+        }
+
+        return $next($request);
+    }
+}

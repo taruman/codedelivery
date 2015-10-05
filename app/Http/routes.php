ca12e78bc899b596310f88161b98f3a204d3e61a
Taruman <taru931@gmail.com>
2015-10-05 02:26:24 -0300

    agrupando rotas e incluindo middleware

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,8 +15,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('admin/categories', ['as' => 'admin.categories.index', 'uses' => 'CategoriesController@index']);
-Route::get('admin/categories/create', ['as' => 'admin.categories.create', 'uses' => 'CategoriesController@create']);
-Route::post('admin/categories/store', ['as' => 'admin.categories.store', 'uses' => 'CategoriesController@store']);
-Route::get('admin/categories/edit/{id}', ['as' => 'admin.categories.edit', 'uses' => 'CategoriesController@edit']);
-Route::post('admin/categories/update/{id}', ['as' => 'admin.categories.update', 'uses' => 'CategoriesController@update']);
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth.checkrole'], function(){
+	Route::get('categories', ['as' => 'categories.index', 'uses' => 'CategoriesController@index']);
+	Route::get('categories/create', ['as' => 'categories.create', 'uses' => 'CategoriesController@create']);
+	Route::post('categories/store', ['as' => 'categories.store', 'uses' => 'CategoriesController@store']);
+	Route::get('categories/edit/{id}', ['as' => 'categories.edit', 'uses' => 'CategoriesController@edit']);
+	Route::post('categories/update/{id}', ['as' => 'categories.update', 'uses' => 'CategoriesController@update']);
+
+	Route::get('products', ['as' => 'products.index', 'uses' => 'ProductsController@index']);
+	Route::get('products/create', ['as' => 'products.create', 'uses' => 'ProductsController@create']);
+	Route::post('products/store', ['as' => 'products.store', 'uses' => 'ProductsController@store']);
+	Route::get('products/edit/{id}', ['as' => 'products.edit', 'uses' => 'ProductsController@edit']);
+	Route::post('products/update/{id}', ['as' => 'products.update', 'uses' => 'ProductsController@update']);
+	Route::get('products/delete/{id}', ['as' => 'products.delete', 'uses' => 'ProductsController@delete']);
+});

31934bfe92b54b207d69abf88978aff99dc2117a
Taruman <taru931@gmail.com>
2015-10-05 02:28:18 -0300

    criando user admin para teste nas seeds

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use CodeDelivery\Models\User;
 
 class UserTableSeeder extends Seeder
 {
@@ -11,7 +12,22 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(\CodeDelivery\Models\User::class, 10)->create()->each(function($u){
+        factory(User::class)->create([
+            'name' => "user",
+            'email' => "user@user.com",
+            'password' => bcrypt(123456),
+            'remember_token' => str_random(10),
+        ]);
+
+        factory(User::class)->create([
+            'name' => "admin",
+            'role' => "admin",
+            'email' => "admin@admin.com",
+            'password' => bcrypt(123456),
+            'remember_token' => str_random(10),
+        ]);
+
+        factory(User::class, 10)->create()->each(function($u){
             $u->client()->save(factory(\CodeDelivery\Models\Client::class)->make());
 
             for ($i = 0; $i <= 5; $i++)

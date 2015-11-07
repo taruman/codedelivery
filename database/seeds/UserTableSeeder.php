8872fa760c1175662f128c8d2129129a087b76e2
Taruman <taru931@gmail.com>
2015-11-07 04:39:53 -0200

    criando client quando criar user nas seeds

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -17,7 +17,7 @@ class UserTableSeeder extends Seeder
             'email' => "user@user.com",
             'password' => bcrypt(123456),
             'remember_token' => str_random(10),
-        ]);
+        ])->client()->save(factory(\CodeDelivery\Models\Client::class)->make());
 
         factory(User::class)->create([
             'name' => "admin",
